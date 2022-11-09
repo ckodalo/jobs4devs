63bacd844859d0c36c3a1dde503da35b5dc55e3e
JeremyKungu <jeremikungu27@gmail.com>
2022-11-09 10:15:33 +0300

    added the card component

---
+++ my-app/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+      <h2>Title</h2>
+      <h3>Organization</h3>
+      <a href='google.com'>Location</a>
+    </div>
+  )
+}
+
+export default Card

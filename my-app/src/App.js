547082ba3378894bcef4260c168f6fcd8a05a574
JeremyKungu <jeremikungu27@gmail.com>
2022-11-09 11:38:52 +0300

    saved data in state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
-// import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import Card from './components/Card';
 
 function App() {
+  const [card, setCard] = useState([])
 
+  useEffect(() => {
+    
   fetch('https://jsonplaceholder.typicode.com/todos')
   .then(response => response.json())
-  .then(json => console.log(json)) 
+  .then(data => setCard(data)) 
+  }, [card]) 
 
   return (
     <div className='cards'>

ab3a0923f6b2f48b30e0002a475a869f4a486be0
JeremyKungu <jeremikungu27@gmail.com>
2022-11-10 09:01:31 +0300

    styling

--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -5,8 +5,8 @@ function Card( {cards}) {
     <div>
       {cards.map((card) => (
         <div key={card.id} className='card'>
-          <h2>{card.title}</h2>
-          <h2>{card.id}</h2>
+          <h2 className='title'>{card.title}</h2>
+          <h2 className='organization'>{card.id}</h2>
         </div>
         
       ))}

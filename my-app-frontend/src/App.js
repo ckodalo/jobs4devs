065110acd21cb761b790876d7fe4838a64184eac
calvin <ckodalo@gmail.com>
2022-11-12 01:20:04 +0300

    post works

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,28 +1,9 @@
-
-//import data from "./data.json";
-import Job from "./components/Job";
-import { useState, useEffect } from "react";
-import Header from "./components/Header";
-import Search from "./components/Search";
+import React from 'react'
 
 function App() {
-  const [jobs, setJobs] = useState([]);
-
-  useEffect(() => {
-    
-    fetch('https://obscure-springs-19515.herokuapp.com/jobs')
-    .then(response => response.json())
-    .then(data => setJobs(data)) 
-    }, []) 
-
-
   return (
-    <div>
-      <div className="header">JOBS 4 DEVS</div>
-      <Job jobs={jobs}/>
-    <div className="footer"></div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
